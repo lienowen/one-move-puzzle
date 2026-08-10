78b33aab36356778c9f668c541d79892db9d6961
lienowen <lienowen@outlook.com>
2026-08-10 17:54:48 +0800

    Route vector mazes to physical runtime

--- src/runtimeRouter.js
+++ src/runtimeRouter.js
@@ -2,9 +2,11 @@ import { getMachineLogic } from './machineLogic.js';
 import { mountWorkshopRuntime } from './workshopRuntime.js';
 import { mountSignalMatchRuntime } from './signalMatchRuntime.js';
 import { mountMazePuzzleRuntime } from './mazePuzzleRuntime.js';
+import { mountMazeVectorRuntime } from './mazeVectorRuntime.js';
 
 export function mountMachineRuntime(options) {
   const logic = getMachineLogic(options.level.id);
+  if (logic?.maze?.mode === 'vector') return mountMazeVectorRuntime(options);
   if (logic?.maze) return mountMazePuzzleRuntime(options);
   if (logic?.archetype === 'signal-match') return mountSignalMatchRuntime(options);
   return mountWorkshopRuntime(options);
