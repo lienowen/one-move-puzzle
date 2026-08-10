9f935dc90f67c708018f05e462e8d2aa5f72d6e8
lienowen <lienowen@outlook.com>
2026-08-10 16:10:32 +0800

    Route maze puzzles to graph runtime

--- src/runtimeRouter.js
+++ src/runtimeRouter.js
@@ -1,9 +1,11 @@
 import { getMachineLogic } from './machineLogic.js';
 import { mountWorkshopRuntime } from './workshopRuntime.js';
 import { mountSignalMatchRuntime } from './signalMatchRuntime.js';
+import { mountMazeRuntime } from './mazeRuntime.js';
 
 export function mountMachineRuntime(options) {
   const logic = getMachineLogic(options.level.id);
+  if (logic?.archetype === 'maze-one-turn') return mountMazeRuntime(options);
   if (logic?.archetype === 'signal-match') return mountSignalMatchRuntime(options);
   return mountWorkshopRuntime(options);
 }
